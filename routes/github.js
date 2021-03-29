6a14fb7a596b889cff71d0e74de85e86151ed281
Jerson Q <solorsanolopez@gmail.com>
2021-03-29 00:07:06 -0700

    [Change] github/oauth-callback router for /code-exchange route

--- routes/github.js
+++ routes/github.js
@@ -1,16 +1,13 @@
 const router = require('express').Router();
 const axios = require('axios');
-const STATE = 2113899652;
 const authUrl = 'https://github.com/login/oauth/authorize';
 const access_token_url = 'https://github.com/login/oauth/access_token';
-const client_redirect_url = 'http://localhost:3000/dashboard/github_section';
 const client_id = process.env.GITHUB_API_CLIENT_ID;
 const client_secret = process.env.GITHUB_API_CLIENT_SECRET;
 const scope = 'repo';
 
-router.get('/oauth-callback', async ({ query: { code } }, res) => {
+router.get('/code-exchange', async ({ query: { code } }, res) => {
   console.log(code);
-  const STATE = 2113899652;
 
   const body = {
     client_id,
@@ -25,10 +22,7 @@ router.get('/oauth-callback', async ({ query: { code } }, res) => {
     .post(access_token_url, body, options)
     .then((response) => response.data.access_token)
     .then((token) => {
-      // eslint-disable-next-line no-console
-      console.log('My token:', token);
-
-      res.redirect(`${client_redirect_url}?token=${token}`);
+      res.status(200).json({ token: token });
     })
     .catch((err) => res.status(500).json({ err: err.message }));
 });
